3d065f7e645465828f02e433f071327166499e3e
Howon Lee <kck4156@kagamine-rin.com>
2025-02-15 23:30:24 +0900

    Bugfix

--- App/App.xaml.cs
+++ App/App.xaml.cs
@@ -7,12 +7,16 @@ using App.Pages;
 namespace App;
 public partial class App : Application
 {
+    public static App Instance { get; private set; }
+
     /// <summary>
     /// Initializes the singleton application object. This is the first line of authored code
     /// executed, and as such is the logical equivalent of main() or WinMain().
     /// </summary>
     public App()
     {
+        Instance = this;
+
         InitializeComponent();
 
         this.UseSegoeMetrics();
--- App/ViewModels/ItemViewModel.cs
+++ App/ViewModels/ItemViewModel.cs
@@ -32,7 +32,7 @@ public partial class ItemViewModel : ObservableObject
 		PriceText = $"{item.Price:N0}";
         var quantityText = Localization.GetLocalizedString("/ItemsPage/ItemViewModelQuantityTemplateText");
         QuantityText = quantityText.Replace("#P1", item.SalesQuantity.ToString("N0")).Replace("#P2", item.StockQuantity.ToString("N0"));
-        QuantityForeground = item.IsSoldout ? new SolidColorBrush(Colors.Red) : null;
+        QuantityForeground = item.IsSoldout ? new SolidColorBrush(Colors.Red) : (SolidColorBrush)App.Instance.Resources.ThemeDictionaries["DefaultTextForegroundThemeBrush"];
     }
 
     [ObservableProperty]
